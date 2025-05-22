1e79b92a1af5c50961e86b7098188a5681c277c8
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-05-22 13:21:03 -0700

    feat: restore loading text in the loader component

--- apps/frontend/components/loader/Loader.tsx
+++ apps/frontend/components/loader/Loader.tsx
@@ -5,7 +5,7 @@ const Loader = () => (
   <div className="tatami-loader-overlay">
     <div className="tatami-loader-content">
       <img src="/Primary%20Logo_Primary%20Color.svg" alt="Tatami Logo" className="tatami-logo" />
-      {/* <span className="tatami-loader-text">Loading...</span> */}
+      <span className="tatami-loader-text">Loading...</span>
     </div>
   </div>
 );
